9aa00c7260ab09fad047640e799d72808d6e7cc8
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 16:39:51 -0300

    Cria o componente Header e move o conteúdo das páginas

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,5 +1,6 @@
 import { Card } from "./Card";
 import { useMatch } from "../../router";
+import { Header } from "../../components/Header";
 
 export function Users() {
   const {
@@ -8,11 +9,7 @@ export function Users() {
 
   return (
     <div>
-      <header>
-        <div className="max-w-7xl mx-auto pb-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Users</h1>
-        </div>
-      </header>
+      <Header>Users</Header>
 
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {users?.map((user) => (
--- src/router.js
+++ src/router.js
@@ -1,18 +1,13 @@
 import { Users } from "./containers/Users/Users";
 import { Link, Outlet, ReactLocation, Router, useMatch } from "react-location";
 import { http } from "./service/api";
+import { Header } from "./components/Header";
 
 const location = new ReactLocation();
 const routes = [
   {
     path: "/",
-    element: (
-      <header>
-        <div className="max-w-7xl mx-auto pb-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-        </div>
-      </header>
-    ),
+    element: <Header>Dashboard</Header>,
   },
   {
     path: "/users",
