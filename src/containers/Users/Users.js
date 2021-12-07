f01c548aed1243382aa8a2367bedb67b4a9165c3
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 16:19:46 -0300

    Move a responsabilidade de trazer os dados para o  nível da rota

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
-import { http } from "../../service/api";
 import { Card } from "./Card";
+import { useMatch } from "../../router";
 
 export function Users() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    http.get("/users").then(({ data }) => setUsers(data.users));
-  }, []);
+  const {
+    data: { users },
+  } = useMatch();
 
   return (
-    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {users?.map((user) => (
-        <Card key={user.id} user={user} />
-      ))}
-    </ul>
+    <div>
+      <header>
+        <div className="max-w-7xl mx-auto pb-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Users</h1>
+        </div>
+      </header>
+
+      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {users?.map((user) => (
+          <Card key={user.id} user={user} />
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/router.js
+++ src/router.js
@@ -1,16 +1,28 @@
 import { Users } from "./containers/Users/Users";
-import { Link, Outlet, ReactLocation, Router } from "react-location";
+import { Link, Outlet, ReactLocation, Router, useMatch } from "react-location";
+import { http } from "./service/api";
 
 const location = new ReactLocation();
 const routes = [
   {
     path: "/",
-    element: <h1>Dashboard</h1>,
+    element: (
+      <header>
+        <div className="max-w-7xl mx-auto pb-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+        </div>
+      </header>
+    ),
   },
   {
     path: "/users",
     element: <Users />,
+    loader: async () => ({
+      users: await http.get("/users").then((data) => {
+        return data.data.users;
+      }),
+    }),
   },
 ];
 
-export { location, routes, Router, Link, Outlet };
+export { location, routes, Router, Link, Outlet, useMatch };
