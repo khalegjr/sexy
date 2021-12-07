17e375b0677e33bbc996c2c38139edfbf6f76480
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 12:27:52 -0300

    Limpa lixo do arquivo

--- src/miragejs/models/index.js
+++ src/miragejs/models/index.js
@@ -2,7 +2,7 @@
  * Mirage JS guide on Models: https://miragejs.com/docs/data-layer/models
  */
 
-import { Model, hasMany, belongsTo } from 'miragejs';
+import { Model } from 'miragejs';
 
 /*
  * Everytime you create a new resource you have
