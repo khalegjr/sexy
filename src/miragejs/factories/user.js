1240c1b69382ba50ca5e6f4f3e7427e04cbb2c3b
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 14:31:28 -0300

    Adiciona um campo de imagem na factory de User

--- src/miragejs/factories/user.js
+++ src/miragejs/factories/user.js
@@ -13,6 +13,9 @@ export default {
     email() {
       return faker.internet.email();
     },
+    imageUrl() {
+      return faker.random.image();
+    },
     role() {
       return faker.random.arrayElement(['admin', 'user']);
     },
