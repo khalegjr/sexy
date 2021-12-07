75ed09bec09ba39afb6802f4367a7ba68f39401b
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 13:28:47 -0300

    Implemente teste para o render de 10 users

--- src/containers/Users/Users.spec.js
+++ src/containers/Users/Users.spec.js
@@ -1,5 +1,24 @@
+import { makeServer } from "../../miragejs/server";
+import { Users } from "./Users";
+import { render, screen, waitFor } from "@testing-library/react";
+
 describe("Containers/Users", () => {
-  it("should render and display 10 users", () => {
-    expect(true).toEqual(true);
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: "test" });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it("should render and display 10 users", async () => {
+    server.createList("user", 10);
+    render(<Users />);
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId("user")).toHaveLength(10);
+    });
   });
 });
