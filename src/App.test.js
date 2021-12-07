651310c5ca07dd7cb3cc156e4e0af45ac3ec0d10
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 11:51:42 -0300

    Altera o teste para usar o servidor Miragejs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { makeServer } from "./miragejs/server";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer();
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  test("App", () => {
+    const users = server.createList("user", 3);
+
+    console.log(users);
+  });
 });
