7a40e07bc23c9f2f4e5971f8df2644af15890e91
Júnior Moreira <junior@adunicamp.org.br>
2021-12-07 19:24:18 -0300

    Altera  a factory e seed para a criação de produtos

---
+++ src/miragejs/factories/products.js
@@ -0,0 +1,20 @@
+import { Factory } from 'miragejs';
+
+/*
+ * Faker Github repository: https://github.com/Marak/Faker.js#readme
+ */
+import faker from 'faker';
+
+export default {
+  product: Factory.extend({
+    name() {
+      return faker.name.findName();
+    },
+    price() {
+      return faker.datatype.number(10000, 1_000_000);
+    },
+    imageUrl() {
+      return faker.random.image();
+    },
+  }),
+};
