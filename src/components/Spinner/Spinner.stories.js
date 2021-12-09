cd4dc630ae77c8c7fe93a1df268fe744cfe1f48a
Júnior Moreira <junior@adunicamp.org.br>
2021-12-09 13:55:28 -0300

    Cria stories para os componentes básicos

---
+++ src/components/Spinner/Spinner.stories.js
@@ -0,0 +1,10 @@
+import { Spinner } from "./Spinner";
+
+export default {
+  title: "Components/Spinner",
+  component: Spinner,
+};
+
+const Template = () => <Spinner />;
+
+export const Default = Template.bind({});
