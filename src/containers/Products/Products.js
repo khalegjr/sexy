31696be72cc5eef91cbf105b36a4fb01f72260ef
Júnior Moreira <junior@adunicamp.org.br>
2021-12-09 11:36:46 -0300

    Altera o componente Products para utilizar Card

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { http } from "../../service/api";
+import { Card } from "./Card";
+import { Header } from "../../components/Header";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
@@ -8,5 +10,15 @@ export default function Products() {
     http.get("/products").then(({ data }) => setProducts(data.products));
   }, []);
 
-  return products.map((product) => <p key={product.id}>{product.name}</p>);
+  return (
+    <div>
+      <Header>Products</Header>
+
+      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products.map((product) => (
+          <Card product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
