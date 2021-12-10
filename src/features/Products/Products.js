02de0a94d0592b6e5e0e23f6e56f00e51c6ba4b1
Júnior Moreira <junior@adunicamp.org.br>
2021-12-10 11:21:04 -0300

    Cria o componente Products e reorganiza as pastas

---
+++ src/features/Products/Products.js
@@ -0,0 +1,16 @@
+import { Card } from "./Card";
+import { Header } from "../../components";
+
+export function Products({ products }) {
+  return (
+    <div>
+      <Header>Products</Header>
+
+      <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products?.map((product) => (
+          <Card product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
